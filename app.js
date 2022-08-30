862e273cb0ef2ac610803b1389a312315bb0f18a
Laskellyb <anikwenzekelly@gmail.com>
2022-08-30 11:25:57 +0100

    trying to work with the api

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const bitCoinData = `{"ticker":{"base":"BTC","target":"USD","price":"20154.98972217","volume":"4646.47299321","change":"23.25950898","markets":[{"market":"BitFinex","price":"20061.00000000","volume":1891.7886384},{"market":"Bitstamp","price":"20056.00000000","volume":865.91893483},{"market":"Cex.io","price":"20091.20000000","volume":29.53915745},{"market":"Exmo","price":"22130.16000000","volume":220.54083777},{"market":"Hitbtc","price":"20059.63000000","volume":0.00481},{"market":"Kraken","price":"20051.00000000","volume":1638.26640364},{"market":"YoBit","price":"20559.80011015","volume":0.41421112}]},"timestamp":1661673607,"success":true,"error":""}`
+const bitCoinData = "https://api.cryptonator.com/api/ticker/btc-usd";
 
-const parsedData = JSON.parse(bitCoinData)
+fetch(bitCoinData)
+  .then(res => res.json())
+  .then(data => console.log(data))
 
-console.log(parsedData.ticker.price)
+// const parsedData = JSON.parse(bitCoinData)
+
+console.log(bitCoinData)
+
+const access_key = 'VGNS7gpTo6o7errodHafToXucwtUhL6p';
+const from = 'GPB';
+const to = 'JPY';
+const amount = 25;
+const url = `https://api.exchangeratesapi.io/latest?access_key=${ access_key }`;
+
+console.log(url)
+
+// const currencyAPIUrl = "https://api.exchangeratesapi.io/v1/latest?access_key=a4fJ9cVqmLvySyOu2QH63JVMThIe9Ydk"
+
+
+// fetch("https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}", requestOptions)
+//   .then(response => response.text())
