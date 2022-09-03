cda75244e2af9163b3a229f118545d75ab85f541
Laskellyb <anikwenzekelly@gmail.com>
2022-09-03 09:18:44 +0100

    Now made the input number value to be multiplied bt the output on the screen

--- app.js
+++ app.js
@@ -1,65 +1,110 @@
 // Variables of select and converting the html collection to an array
-const coinsToExchange = document.getElementById('crypto-id').children;
-const coinIdArray = Array.from(coinsToExchange)
-console.log(coinIdArray[2].innerText)
-const selectCryptoId = document.getElementById('crypto-id');
-
+const coinsToExchange = document.getElementById("crypto-id").children;
+const coinIdArray = Array.from(coinsToExchange);
+console.log(coinIdArray[2].innerText);
+console.log(coinIdArray.selected);
+const selectCryptoId = document.getElementById("crypto-id");
 
 // Get bitcoin data from nomic
-const bitCoinData = "https://api.nomics.com/v1/currencies/ticker?key=23ac2761382825f70678666ea03f9ccdafe7bed4&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&platform-currency=ETH&per-page=100&page=1";
+const bitCoinData =
+  "https://api.nomics.com/v1/currencies/ticker?key=23ac2761382825f70678666ea03f9ccdafe7bed4&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&platform-currency=ETH&per-page=100&page=1";
 
 // Use async to fetch the bitcoin data
-setInterval(()=>{
-  async function getBitcoinFunction(){
-    const fetchData = await fetch(bitCoinData)
-    const data = await fetchData.json()
+// setInterval(() => {
+async function getBitcoinFunction() {
+  const fetchData = await fetch(bitCoinData);
+  const data = await fetchData.json();
 
-    const roundOffCryptoData = Math.round(data[0].price)
-    const bitCoinPrice = document.getElementById('bitcoin-price')
-    bitCoinPrice.innerText = roundOffCryptoData
+  const roundOffCryptoData = Math.round(data[0].price);
 
-    // Show time
-    let timeToday = new Date;
-    // const timeToday = data[0].price_date;
-    const showDate = document.getElementById('show-date');
-    showDate.innerText = timeToday
-  }
-  getBitcoinFunction()
-}, 1000)
+  // Show time
+  let timeToday = new Date();
+  const showDate = document.getElementById("show-date");
+  showDate.innerText = timeToday;
+
+  // Currency conversion from exchangeAPI
+  const access_key = "VGNS7gpTo6o7errodHafToXucwtUhL6p";
+  const from = "USD";
+  const to = "NGN";
+  const amount = roundOffCryptoData;
+
+  const myHeaders = new Headers();
+  myHeaders.append("apikey", "VGNS7gpTo6o7errodHafToXucwtUhL6p");
+
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    headers: myHeaders,
+  };
+
+  // Variable to append fetched conversion data to
+  let fetchedData = {};
+
+  fetch(
+    `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`,
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log((fetchedData = result)))
+    .catch((error) => console.log("error", error));
+
+  // To delay until the fetch request has been made and convert to JS object : Display on screen
+  setTimeout(() => {
+    const convertData = JSON.parse(fetchedData);
+
+    const nairaId = document.getElementById("naira-id");
+    const usdId = document.getElementById("usd-id");
+
+    const roundOffDecimal = Math.round(convertData.result);
+
+    const getValueOfCoin = document.getElementById("input-coin").value;
+
+    // Multiply with the value from the number input
+    const multiplyInnerNairaText = roundOffDecimal * parseInt(getValueOfCoin);
+
+    const multiplyInnerDollarText = amount * parseInt(getValueOfCoin);
+    console.log(multiplyInnerNairaText);
+
+    nairaId.innerText = `N${multiplyInnerNairaText}`;
+    usdId.innerText = `$${multiplyInnerDollarText}`;
+  }, 3000);
+}
+getBitcoinFunction();
+// }, 4000);
 
 // Convert from btc to currency
 
-// Currency conversion from exchangeAPI
-const access_key = 'VGNS7gpTo6o7errodHafToXucwtUhL6p';
-const from = 'USD';
-const to = 'NGN';
-const amount = 25;
+// // Currency conversion from exchangeAPI
+// const access_key = 'VGNS7gpTo6o7errodHafToXucwtUhL6p';
+// const from = 'USD';
+// const to = 'NGN';
+// const amount = roundOffCryptoData;
 
-const myHeaders = new Headers();
-myHeaders.append("apikey", "VGNS7gpTo6o7errodHafToXucwtUhL6p");
+// const myHeaders = new Headers();
+// myHeaders.append("apikey", "VGNS7gpTo6o7errodHafToXucwtUhL6p");
 
-const requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
+// const requestOptions = {
+//   method: 'GET',
+//   redirect: 'follow',
+//   headers: myHeaders
+// };
 
-// Variable to append fetched conversion data to
-let fetchedData = {};
+// // Variable to append fetched conversion data to
+// let fetchedData = {};
 
-fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(fetchedData = result))
-  .catch(error => console.log('error', error));
+// fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`, requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(fetchedData = result))
+//   .catch(error => console.log('error', error));
 
-  // To delay until the fetch request has been made and convert to JS object : Display on screen
- setTimeout(()=>{
-  const convertData = JSON.parse(fetchedData)
+//   // To delay until the fetch request has been made and convert to JS object : Display on screen
+//  setTimeout(()=>{
+//   const convertData = JSON.parse(fetchedData)
 
-  const nairaId = document.getElementById('naira-id');
-  const usdId = document.getElementById('usd-id');
+//   const nairaId = document.getElementById('naira-id');
+//   const usdId = document.getElementById('usd-id');
 
-  const roundOffNairaDecimal = Math.round(convertData.result)
-  nairaId.innerText = `Naira: ${roundOffNairaDecimal}`
-  usdId.innerText = `USD: $${amount}`
- }, 3000)
+//   const roundOffNairaDecimal = Math.round(convertData.result)
+//   nairaId.innerText = `Naira: ${roundOffNairaDecimal}`
+//   usdId.innerText = `USD: $${amount}`
+//  }, 3000)
